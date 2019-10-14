9ee9eb266a8143c18ad8183a4d43b397a8f93b15
Bala <bala@bserver.com>
2019-10-13 21:28:43 -0400

    initial commit

---
+++ docker1/src/main/java/com/bala/docker1/service/HomeController.java
@@ -0,0 +1,13 @@
+package com.bala.docker1.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello docker world!";
+	}
+}
